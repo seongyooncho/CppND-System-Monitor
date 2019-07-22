032348f228f993d1d27b5a353610d08427b24720
Seongyoon Cho <seongyoon.cho@ixdl.kr>
2019-07-22 09:48:04 +0000

    path.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
 #include "ncurses_display.h"
 #include "system.h"
+#include <cstring>
+#include <iostream>
+#include "Path.h"
 
 int main() {
+  std::cout << "listing: " + Path::basePath() << std::endl;
+  std::string command = "ls " + Path::basePath();
+  int n = command.length();
+  char char_command[n+1];
+  strcpy(char_command, command.c_str());
+  std::system(char_command);
+  return 0;
+
   System system;
   NCursesDisplay::Display(system);
 }
